8c0424c4afb5a34dec1670cbbf21573047b8cce2
AnnaZimina <xadea@yandex.ru>
2022-08-18 11:10:20 +0300

    Task 4

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// программa, которая задаёт массив из N элементов 
+// и выводит их на экран
+
+Console.Clear();
+
+int[] bigarray(int[] array)
+{
+    for(int i=0; i < array.Length; i++)
+    {
+    Console.Write("Input number ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void Display(int[] array)
+{
+    Console.Write("[ ");
+
+    for (int j = 0; j<array.Length-1; j++)
+        {
+            Console.Write(array[j] + ", ");
+        }
+
+    Console.Write( array[array.Length-1] + " ]");
+}
+
+Console.Write("How many numbers in your array? ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] userarray = new int [length];
+
+bigarray(userarray);
+Display(userarray);
