62e94f1d7c1a1aea20b1671eab716dc6c43827ea
AnnaZimina <xadea@yandex.ru>
2022-08-18 11:06:48 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа, которая принимает на вход число
+// и выдаёт сумму цифр в числе
+
+Console.Clear();
+
+Console.WriteLine("Input number");
+double digit = Convert.ToInt32(Console.ReadLine());
+double count = 0;
+while (digit > 0)
+{
+    count = count + digit % 10;
+    digit = (digit - digit % 10)/10;
+}
+Console.WriteLine("Sum of all number's digits is " + count);
